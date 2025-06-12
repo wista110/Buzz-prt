d84d5c591b9018dd2a495d90672eac41c035eae9
wista110 <wista.dev1@gmail.com>
2025-06-12 23:21:44 +0900

    faviconの全ブラウザ対応

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,13 @@ export default function Home() {
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://Buzz-prt.com" />
         <meta name="twitter:card" content="summary_large_image" />
-        <link rel="icon" href="/favicon.png" />
+        
+        {/* ファビコン設定（複数サイズ対応） */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         
         {/* Google Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
